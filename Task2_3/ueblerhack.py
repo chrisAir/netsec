8c252b96cd6f3d3e97e4452eed31a1a56a245032
Christian Stark <stark@cs.uni-bonn.de>
2016-05-05 13:49:15 +0200

    trim string

--- Task2_3/ueblerhack.py
+++ Task2_3/ueblerhack.py
@@ -2,8 +2,12 @@ path = 'rfc3093.txt'
 
 file = open(path, 'r')
 
+removechars = ['.', ',', '[', ']', '(', ')', '{', '}', '"']
+sc = set(removechars)
+
 for line in file:
     for word in line.split():
+        word = ''.join([c for c in word if c not in sc])
         print word
 
 
