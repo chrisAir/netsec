136383ccb335f74267dbc0e77334562405697655
ronaldbrenner <ronaldbrenner@web.de>
2016-07-05 12:32:32 +0200

    Aufgabe 2&6

--- Sheet 05/Task5_4/OTPScript.sh
+++ Sheet 05/Task5_4/OTPScript.sh
@@ -2,6 +2,6 @@
 
 otp="$(head -c 500 /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w 20 | head -n 1)"
 
-java -jar ./out/artifacts/OTPServer_jar/OTPServer.jar ${otp} &
+java -jar ./jarFiles/OTPServer.jar ${otp} &
 
-java -jar ./out/artifacts/OTPClient_jar/OTPClient.jar ${otp} &
+java -jar ./jarFiles/OTPClient.jar ${otp} &
